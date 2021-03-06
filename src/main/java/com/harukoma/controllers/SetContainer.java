0683920276b11fd139598fb288711156c02e40c2
valerie yip <valerieyipfh@gmail.com>
2021-03-06 20:52:44 +0800

    added set entity and bootstrap so that it is retrievable by frontend

---
+++ src/main/java/com/harukoma/controllers/SetContainer.java
@@ -0,0 +1,44 @@
+package com.harukoma.controllers;
+
+import com.harukoma.domain.Card;
+
+import java.util.List;
+
+public class SetContainer {
+    private List<Card> cards;
+    private String name;
+    private Long id;
+    private String description;
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
